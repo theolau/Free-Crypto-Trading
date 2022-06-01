763bad3b58500053b5152f86705fd9dbc2038889
Theo <theo313231@gmail.com>
2022-05-31 21:53:48 -0700

    added api key, gitignore

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function getApi() {
+  console.log(config);
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", config.API_KEY);
+  
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  fetch("https://api.apilayer.com/currency_data/change?start_date=2018-08-08&end_date=2018-08-15", requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Use the console to examine the response
+      console.log(data);
+      // TODO: Loop through the data and generate your HTML
+    })
+    .catch(error => console.log('error', error));
+}
+getApi();
